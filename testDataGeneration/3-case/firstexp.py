3d2b5db874e6f3b2965cd1c878802620762864f5
avi028 <math28arnav@gmail.com>
2023-10-10 12:07:38 +0530

    modified the test data generation

--- testDataGeneration/3-case/firstexp.py
+++ testDataGeneration/3-case/firstexp.py
@@ -4,11 +4,11 @@ key_list = ["name", "country", "institute", "id", "course", "program", "city", "
 if __name__ == "__main__":
     f = open("exp1.json", "w")
     f.write("{")
-    value_size = 10
+    value_size = 12
     
     for i in key_list:
         f.write('"' + i + '": "')
-        for j in range(1, value_size):
+        for j in range(0, value_size):
             f.write("1")
         if i == key_list[len(key_list) - 1]:
             f.write('"}')
